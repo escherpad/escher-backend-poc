4d9ec277c1cf039470e78d3b9b2a1fc9c16877ea
Ge Yang <yangge1987@gmail.com>
2016-08-07 02:04:05 -0700

    add rust server

--- go-chat-server/src/main.go
+++ go-chat-server/src/main.go
@@ -1,47 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+	"io"
+	//"net/http"
 
-	"github.com/googollee/go-socket.io"
+	"golang.org/x/net/websocket"
+	"net/http"
 )
 
+
 func main() {
-	server, err := socketio.NewServer(nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	server.On("connection", func(so socketio.Socket) {
-		log.Println("on connection")
-		so.Join("chat")
-		so.On("chat message", func(msg string) {
-			//这个没有问题
-			fmt.Println("chat message")
-			e := so.Emit("cn2222", "中文2")
-			log.Println(e)
-
-			log.Println("emit:", so.Emit("chat message", "中文输入法"))
-			so.BroadcastTo("chat", "chat message", msg)
-		})
-		// Socket.io acknowledgement example
-		// The return type may vary depending on whether you will return
-		// For this example it is "string" type
-		so.On("chat message with ack", func(msg string) string {
-			fmt.Println("chat message with ack")
-			return msg
-		})
-		so.On("disconnection", func() {
-			log.Println("on disconnect")
-		})
-	})
-	server.On("error", func(so socketio.Socket, err error) {
-		log.Println("error:", err)
-	})
-
-	http.Handle("/socket.io/", server)
-	http.Handle("/", http.FileServer(http.Dir("./asset")))
-	log.Println("Serving at localhost:5000...")
-	log.Fatal(http.ListenAndServe(":5000", nil))
+	http.Handle("/", websocket.Handler(echoHandler))
+	//http.Handle("/", http.FileServer(http.Dir(".")))
+	//err := http.ListenAndServe(":8080", nil)
+	//if err != nil {
+	//	panic("ListenAndServe: " + err.Error())
+	//}
 }
